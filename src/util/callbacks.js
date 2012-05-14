269bf1d898b091d53361a98b723019186b5c1f4d
Mikael Karon <mikael@karon.se>
2012-05-14 15:39:59 +0800

    remove utils

--- src/util/callbacks.js
+++
@@ -1,8 +0,0 @@
-/*!
- * TroopJS callbacks component
- * @license TroopJS 0.0.1 Copyright 2012, Mikael Karon <mikael@karon.se>
- * Released under the MIT license.
- */
-define([ "jquery" ], function CallbacksModule($) {
-	return $.Callbacks;
-});
