6b38cdff06b9f59414c976c6012a535748645737
Mikael Karon <mikael@karon.se>
2012-06-28 18:02:44 +0800

    Since re has g flag, don't reuse
    Simplify comparator
    
    Simplify dimension iterator
    
    Update header and comments

--- src/dimensions.js
+++ src/dimensions.js
@@ -4,7 +4,6 @@
  * Released under the MIT license.
  */
 define([ "jquery" ], function DimensionsModule($) {
-	var RE = /(w|h)(\d*)/g;
 	var DIMENSIONS = "dimensions";
 	var RESIZE = "resize." + DIMENSIONS;
 	var W = "w";
@@ -16,27 +15,39 @@ define([ "jquery" ], function DimensionsModule($) {
 	 * Internal comparator used for reverse sorting arrays
 	 */
 	function reverse(a, b) {
-		return a < b ? 1 : a > b ? -1 : 0;
+		return b - a;
 	}
 
+	/**
+	 * Internal onResize handler
+	 * @param $event
+	 */
 	function onResize($event) {
 		var $self = $(this);
-		var w = $self.width();
-		var h = $self.height();
+		var width = $self.width();
+		var height = $self.height();
 
-		$.each($self.data(DIMENSIONS), function dimensionIterator(namespace, dimension) {
-			var dimension_w = dimension[W];
-			var dimension_w_max = dimension_w.length - 1;
-			var dimension_h = dimension[H];
-			var dimension_h_max = dimension_h.length - 1;
+		// Iterate all dimensions
+		$.each($.data(self, DIMENSIONS), function dimensionIterator(namespace, dimension) {
+			var w = dimension[W];
+			var h = dimension[H];
+			var _w;
+			var _h;
+			var i;
 
-			var _w = $.grep(dimension_w, function(_w, i) {
-				return _w <= w || i === dimension_w_max;
-			})[0];
-			var _h = $.grep(dimension_h, function(_h, i) {
-				return _h <= h || i === dimension_h_max;
-			})[0];
+			i = w.length;
+			_w = w[i - 1];
+			while(w[--i] < width) {
+				_w = w[i];
+			}
 
+			i = h.length;
+			_h = h[i - 1];
+			while(h[--i] < height) {
+				_h = h[i];
+			}
+
+			// If _w or _h has changed, update and trigger
 			if (_w !== dimension[_W] || _h !== dimension[_H]) {
 				dimension[_W] = _w;
 				dimension[_H] = _h;
@@ -62,23 +73,33 @@ define([ "jquery" ], function DimensionsModule($) {
 				.data(DIMENSIONS, {});
 		},
 
+		/**
+		 * Do something each time an event handler is bound to a particular element
+		 * @param handleObj (Object)
+		 */
 		add : function onDimensionsAdd(handleObj) {
+			var self = this;
 			var namespace = handleObj.namespace;
 			var dimension = {};
 			var w = dimension[W] = [];
 			var h = dimension[H] = [];
+			var re = /(w|h)(\d+)/g;
 			var matches;
 
-			while (matches = RE.exec(namespace)) {
+			while (matches = re.exec(namespace)) {
 				dimension[matches[1]].push(parseInt(matches[2]));
 			}
 
 			w.sort(reverse);
 			h.sort(reverse);
 
-			$.data(this, DIMENSIONS)[namespace] = dimension;
+			$.data(self, DIMENSIONS)[namespace] = dimension;
 		},
 
+		/**
+		 * Do something each time an event handler is unbound from a particular element
+		 * @param handleObj (Object)
+		 */
 		remove : function onDimensionsRemove(handleObj) {
 			delete $.data(this, DIMENSIONS)[handleObj.namespace];
 		},
