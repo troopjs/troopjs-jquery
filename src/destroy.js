531fc6da784fa75aa9221bcf9ec392ab54923e0f
Mikael Karon <mikael@karon.se>
2012-07-13 09:46:20 +0800

    fix headers

--- src/destroy.js
+++ src/destroy.js
@@ -1,6 +1,6 @@
 /*!
  * TroopJS jQuery destroy plug-in
- * @license TroopJS 0.0.1 Copyright 2012, Mikael Karon <mikael@karon.se>
+ * @license TroopJS Copyright 2012, Mikael Karon <mikael@karon.se>
  * Released under the MIT license.
  */
 define([ "jquery" ], function DestroyModule($) {
