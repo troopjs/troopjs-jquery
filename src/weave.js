7ecd372cbe7907c4e78013ac95ee6b1236116685
Mikael Karon <mikael@karon.se>
2012-07-18 17:02:54 +0800

    use pending during weave/unweave

--- src/weave.js
+++ src/weave.js
@@ -59,94 +59,96 @@ define([ "jquery" ], function WeaveModule($) {
 				var woven = $data[WOVEN] = [];
 				var pending = $data[PENDING] || ($data[PENDING] = []);
 				var re = /[\s,]*([\w_\-\/]+)(?:\(([^\)]+)\))?/g;
-				var mark = i;
 				var j = 0;
 				var matches;
 
-				$element
-					// Make sure to remove DATA_WEAVE (so we don't try processing this again)
-					.removeAttr(DATA_WEAVE)
-					// Bind destroy event
-					.bind(DESTROY, onDestroy);
+				// Wait for all pending deferred
+				$WHEN.apply($, pending).done(function doneRequired() {
+					$element
+						// Make sure to remove DATA_WEAVE (so we don't try processing this again)
+						.removeAttr(DATA_WEAVE)
+						// Bind destroy event
+						.bind(DESTROY, onDestroy);
 
-				// Iterate woven (while the RE_WEAVE matches)
-				while (matches = re.exec(weave)) {
-					// Defer weave
-					$.Deferred(function deferredRequire(dfdWeave) {
-						var _j = j++; // store _j before we increment
-						var k;
-						var l;
-						var kMax;
-						var value;
+					// Iterate woven (while the RE_WEAVE matches)
+					while (matches = re.exec(weave)) {
+						// Defer weave
+						$.Deferred(function deferredRequire(dfdWeave) {
+							var _j = j++; // store _j before we increment
+							var k;
+							var l;
+							var kMax;
+							var value;
 
-						// Store on woven
-						widgets[i++] = dfdWeave;
+							// Add to widgets and pending
+							widgets[i++] = pending[pending.length] = dfdWeave;
 
-						// Link deferred
-						dfdWeave.then(function doneRequire(widget) {
-							woven[_j] = widget;
-						}, deferred.reject, deferred.notify);
+							// Link deferred
+							dfdWeave.then(function doneRequire(widget) {
+								woven[_j] = widget;
+							}, deferred.reject, deferred.notify);
 
-						// Get widget name
-						var name = matches[1];
+							// Get widget name
+							var name = matches[1];
 
-						// Set initial argv
-						var argv = [ $element, name ];
+							// Set initial argv
+							var argv = [ $element, name ];
 
-						// Append values from arg to argv
-						for (k = 0, kMax = arg.length, l = argv.length; k < kMax; k++, l++) {
-							argv[l] = arg[k];
-						}
+							// Append values from arg to argv
+							for (k = 0, kMax = arg.length, l = argv.length; k < kMax; k++, l++) {
+								argv[l] = arg[k];
+							}
 
-						// Get widget args
-						var args = matches[2];
+							// Get widget args
+							var args = matches[2];
 
-						// Any widget arguments
-						if (args !== UNDEFINED) {
-							// Convert args to array
-							args = args.split(RE_SEPARATOR);
+							// Any widget arguments
+							if (args !== UNDEFINED) {
+								// Convert args to array
+								args = args.split(RE_SEPARATOR);
 
-							// Append typed values from args to argv
-							for (k = 0, kMax = args.length, l = argv.length; k < kMax; k++, l++) {
-								// Get value
-								value = args[k];
+								// Append typed values from args to argv
+								for (k = 0, kMax = args.length, l = argv.length; k < kMax; k++, l++) {
+									// Get value
+									value = args[k];
 
-								if (value in $data) {
-									argv[l] = $data[value];
-								} else if (RE_STRING.test(value)) {
-									argv[l] = value.slice(1, -1);
-								} else if (RE_DIGIT.test(value)) {
-									argv[l] = Number(value);
-								} else if (RE_BOOLEAN.test(value)) {
-									argv[l] = RE_BOOLEAN_TRUE.test(value);
-								} else {
-									argv[l] = value;
+									if (value in $data) {
+										argv[l] = $data[value];
+									} else if (RE_STRING.test(value)) {
+										argv[l] = value.slice(1, -1);
+									} else if (RE_DIGIT.test(value)) {
+										argv[l] = Number(value);
+									} else if (RE_BOOLEAN.test(value)) {
+										argv[l] = RE_BOOLEAN_TRUE.test(value);
+									} else {
+										argv[l] = value;
+									}
 								}
 							}
-						}
 
-						require([ name ], function required(Widget) {
-							// Defer require
-							$.Deferred(function deferredStart(dfdRequire) {
-								// Constructed and initialized instance
-								var widget = Widget.apply(Widget, argv);
+							require([ name ], function required(Widget) {
+								// Defer require
+								$.Deferred(function deferredStart(dfdRequire) {
+									// Constructed and initialized instance
+									var widget = Widget.apply(Widget, argv);
 
-								// Link deferred
-								dfdRequire.then(function doneStart() {
-									dfdWeave.resolve(widget);
-								}, dfdWeave.reject, dfdWeave.notify);
+									// Link deferred
+									dfdRequire.then(function doneStart() {
+										dfdWeave.resolve(widget);
+									}, dfdWeave.reject, dfdWeave.notify);
 
-								// Start
-								widget.start(dfdRequire);
+									// Start
+									widget.start(dfdRequire);
+								});
 							});
 						});
-					});
-				}
+					}
 
-				// Slice out woven woven for this element
-				$WHEN.apply($, widgets.slice(mark, i)).done(function doneRequired() {
-					// Set DATA_WOVEN attribute
-					$element.attr(DATA_WOVEN, JOIN.call(arguments, " "));
+					// Slice out woven woven for this element
+					$WHEN.apply($, pending).done(function donePending() {
+						// Set DATA_WOVEN attribute
+						$element.attr(DATA_WOVEN, JOIN.call(arguments, " "));
+					});
 				});
 			});
 
@@ -161,6 +163,7 @@ define([ "jquery" ], function WeaveModule($) {
 		var i = 0;
 		var $elements = $(this);
 
+		// Create default deferred if none was passed
 		deferred = deferred || $.Deferred();
 
 		$elements
@@ -169,33 +172,39 @@ define([ "jquery" ], function WeaveModule($) {
 			// Iterate
 			.each(function elementIterator(index, element) {
 				var $element = $(element);
-				var widgets = $element.data(WOVEN);
+				var $data = $element.data();
+				var pending = $data[PENDING];
+				var woven = $data[WOVEN];
 				var widget;
 
-				$element
+				$WHEN.apply($, pending).done(function donePending() {
 					// Remove WOVEN data
-					.removeData(WOVEN)
-					// Remove DATA_WOVEN attribute
-					.removeAttr(DATA_WOVEN);
+					delete $data[WOVEN];
 
-				// Somewhat safe(r) iterator over widgets
-				while (widget = widgets.shift()) {
-					// $.Deferred stop
-					$.Deferred(function deferredStop(dfdStop) {
-						// Store on unwoven
-						unwoven[i++] = dfdStop;
+					$element
+						// Remove DATA_WOVEN attribute
+						.removeAttr(DATA_WOVEN);
 
-						widget.stop(dfdStop);
-					});
-				}
+					// Somewhat safe(r) iterator over woven
+					while (widget = woven.shift()) {
+						// $.Deferred stop
+						$.Deferred(function deferredStop(dfdStop) {
+							// Add to unwoven and pending
+							unwoven[i++] = pending[pending.length] = dfdStop;
 
-				$element
-					// Copy woven data to data-weave attribute
-					.attr(DATA_WEAVE, $element.data(WEAVE))
+							widget.stop(dfdStop);
+						});
+					}
+
+					$element
+						// Copy weave data to data-weave attribute
+						.attr(DATA_WEAVE, $data[WEAVE])
+						// Make sure to clean the destroy event handler
+						.unbind(DESTROY, onDestroy);
+
 					// Remove data fore WEAVE
-					.removeData(WEAVE)
-					// Make sure to clean the destroy event handler
-					.unbind(DESTROY, onDestroy);
+					delete $data[WEAVE];
+				});
 			});
 
 		if (deferred) {
